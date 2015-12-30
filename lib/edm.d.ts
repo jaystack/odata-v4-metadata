c5f6d12b0b235b5a9cef8328530db002bb55262d
Peter Aron Zentai <peter.zentai@avangrade.com>
2015-12-30 03:40:33 +0100

    more classes from edm

--- lib/edm.d.ts
+++ lib/edm.d.ts
@@ -2,42 +2,43 @@ export declare namespace Edm {
     class PrimitiveType {
         className: string;
         constructor(className: string);
+        toString(): string;
     }
-    let Binary: PrimitiveType;
-    let Boolean: PrimitiveType;
-    let Byte: PrimitiveType;
-    let Date: PrimitiveType;
-    let DateTimeOffset: PrimitiveType;
-    let Decimal: PrimitiveType;
-    let Double: PrimitiveType;
-    let Duration: PrimitiveType;
-    let Guid: PrimitiveType;
-    let Int16: PrimitiveType;
-    let Int32: PrimitiveType;
-    let Int64: PrimitiveType;
-    let SByte: PrimitiveType;
-    let Single: PrimitiveType;
-    let Stream: PrimitiveType;
-    let String: PrimitiveType;
-    let TimeOfDay: PrimitiveType;
-    let Geography: PrimitiveType;
-    let GeographyPoint: PrimitiveType;
-    let GeographyLineString: PrimitiveType;
-    let GeographyPolygon: PrimitiveType;
-    let GeographyMultiPoint: PrimitiveType;
-    let GeographyMultiLineString: PrimitiveType;
-    let GeographyMultiPolygon: PrimitiveType;
-    let GeographyCollection: PrimitiveType;
-    let Geometry: PrimitiveType;
-    let GeometryPoint: PrimitiveType;
-    let GeometryLineString: PrimitiveType;
-    let GeometryPolygon: PrimitiveType;
-    let GeometryMultiPoint: PrimitiveType;
-    let GeometryMultiLineString: PrimitiveType;
-    let GeometryMultiPolygon: PrimitiveType;
-    let GeometryCollection: PrimitiveType;
+    const Binary: PrimitiveType;
+    const Boolean: PrimitiveType;
+    const Byte: PrimitiveType;
+    const Date: PrimitiveType;
+    const DateTimeOffset: PrimitiveType;
+    const Decimal: PrimitiveType;
+    const Double: PrimitiveType;
+    const Duration: PrimitiveType;
+    const Guid: PrimitiveType;
+    const Int16: PrimitiveType;
+    const Int32: PrimitiveType;
+    const Int64: PrimitiveType;
+    const SByte: PrimitiveType;
+    const Single: PrimitiveType;
+    const Stream: PrimitiveType;
+    const String: PrimitiveType;
+    const TimeOfDay: PrimitiveType;
+    const Geography: PrimitiveType;
+    const GeographyPoint: PrimitiveType;
+    const GeographyLineString: PrimitiveType;
+    const GeographyPolygon: PrimitiveType;
+    const GeographyMultiPoint: PrimitiveType;
+    const GeographyMultiLineString: PrimitiveType;
+    const GeographyMultiPolygon: PrimitiveType;
+    const GeographyCollection: PrimitiveType;
+    const Geometry: PrimitiveType;
+    const GeometryPoint: PrimitiveType;
+    const GeometryLineString: PrimitiveType;
+    const GeometryPolygon: PrimitiveType;
+    const GeometryMultiPoint: PrimitiveType;
+    const GeometryMultiLineString: PrimitiveType;
+    const GeometryMultiPolygon: PrimitiveType;
+    const GeometryCollection: PrimitiveType;
     class EdmItemBase {
-        private parent;
+        parent: EdmItemBase;
         constructor(definition?: any, parent?: EdmItemBase);
         loadFrom(definition: any): void;
     }
@@ -80,5 +81,64 @@ export declare namespace Edm {
         hasStream: boolean;
         properties: Array<Property>;
         navigationProperties: Array<NavigationProperty>;
+    }
+    class ComplexType extends EdmItemBase {
+        name: string;
+        baseType: string;
+        abstract: boolean;
+        openType: boolean;
+        hasStream: boolean;
+        properties: Array<Property>;
+        navigationProperties: Array<NavigationProperty>;
+    }
+    class Parameter extends EdmItemBase {
+        name: string;
+        type: string;
+        nullable: boolean;
+        maxLength: number;
+        precision: number;
+        scale: number;
+        unicode: boolean;
+        SRID: number;
+    }
+    class ReturnType extends EdmItemBase {
+        type: string;
+        nullable: boolean;
+    }
+    class Invokable extends EdmItemBase {
+    }
+    class Action extends Invokable {
+        name: string;
+        isBound: boolean;
+        entitySetPath: string;
+        parameters: Array<Parameter>;
+        returnType: ReturnType;
+    }
+    class Function extends Invokable {
+        name: string;
+        isBound: boolean;
+        entitySetPath: string;
+        parameters: Array<Parameter>;
+        returnType: ReturnType;
+        isComposable: boolean;
+    }
+    class Member extends EdmItemBase {
+        name: string;
+        value: number;
+    }
+    class EnumType extends EdmItemBase {
+        namespace: string;
+        underlyingType: PrimitiveType;
+        isFlags: boolean;
+        members: Array<Member>;
+    }
+    class Schema extends EdmItemBase {
+        namespace: string;
+        alias: string;
+        enumTypes: Array<EnumType>;
+        complexTypes: Array<ComplexType>;
+        entityTypes: Array<EntityType>;
+        actions: Array<Action>;
+        functions: Array<Edm.Function>;
     }
 }
